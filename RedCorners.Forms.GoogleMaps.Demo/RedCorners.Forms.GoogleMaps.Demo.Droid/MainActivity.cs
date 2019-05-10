1a0946c57e579cc73c69155de0220543b9e24021
Saeed Afshari (NEATBOOK_OSX) <s_a@live.com>
2019-05-10 20:49:58 +0200

    Android Demo project (work in progress)

---
+++ RedCorners.Forms.GoogleMaps.Demo/RedCorners.Forms.GoogleMaps.Demo.Droid/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Xamarin.Forms;
+using Android.Content.PM;
+
+namespace RedCorners.Forms.GoogleMaps.Demo.Droid
+{
+    [Activity(Label = "RedCorners.Forms.GoogleMaps.Demo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.FormsGoogleMaps.Init(this, savedInstanceState);
+
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] global::Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
