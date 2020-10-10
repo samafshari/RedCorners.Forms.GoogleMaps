a84340f6b41bd26fb47582813a74e02f18810a66
s_a@live.com (NEATBOOK2) <s_a@live.com>
2020-10-10 23:27:15 +0200

    fix syntax error

--- RedCorners.Forms.GoogleMaps/RedCorners.Forms.GoogleMaps.Shared/Systems/MapLocationSystem.cs
+++ RedCorners.Forms.GoogleMaps/RedCorners.Forms.GoogleMaps.Shared/Systems/MapLocationSystem.cs
@@ -55,10 +55,10 @@ namespace RedCorners.Forms.GoogleMaps
 			var l2 = new CoreLocation.CLLocation(p2.Latitude, p2.Longitude);
             return l1.DistanceFrom(l2);
 #else
-            var d1 = latitude * (Math.PI / 180.0);
-            var num1 = longitude * (Math.PI / 180.0);
-            var d2 = otherLatitude * (Math.PI / 180.0);
-            var num2 = otherLongitude * (Math.PI / 180.0) - num1;
+            var d1 = p1.Latitude * (Math.PI / 180.0);
+            var num1 = p1.Longitude * (Math.PI / 180.0);
+            var d2 = p2.Latitude * (Math.PI / 180.0);
+            var num2 = p2.Longitude * (Math.PI / 180.0) - num1;
             var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) + Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
 
             return 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
