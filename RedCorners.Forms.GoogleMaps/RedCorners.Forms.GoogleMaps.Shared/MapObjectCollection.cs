61357b711c6416a86493b18afdf0d3f11a7a665b
s_a@live.com (NEATBOOK2) <s_a@live.com>
2020-10-11 17:37:47 +0200

    MapObjectCollectionBase

---
+++ RedCorners.Forms.GoogleMaps/RedCorners.Forms.GoogleMaps.Shared/MapObjectCollection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedCorners.Forms.GoogleMaps
+{
+    public class MapObjectCollection : MapObjectCollectionBase
+    {
+    }
+}
---
+++ RedCorners.Forms.GoogleMaps/RedCorners.Forms.GoogleMaps.Shared/MapObjectCollectionBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedCorners.Forms.GoogleMaps
+{
+    public abstract class MapObjectCollectionBase : MapObject
+    {
+        public delegate void CollectionChangeDelegate(MapObjectCollectionBase collection);
+
+        public event CollectionChangeDelegate CollectionChanged;
+
+        public virtual Task<IEnumerable<MapObject>> GetItemsAsync()
+        {
+            throw new Exception("Do not call base on GetItemsAsync()");
+        }
+
+        public virtual async Task<IEnumerable<MapObject>> GetVisibleItemsAsync(MapRegion region)
+        {
+            return 
+                (await GetItemsAsync())
+                .Where(x => x.NeverCull ||!x.ShouldCull(region));
+        }
+
+        public virtual async Task<IEnumerable<MapObject>> GetVisibleItemsAsync(Position center, Distance distance)
+        {
+            return
+                (await GetItemsAsync())
+                .Where(x => x.NeverCull || !x.ShouldCull(center, distance));
+        }
+
+        public void TriggerCollectionChange()
+        {
+            CollectionChanged?.Invoke(this);
+        }
+    }
+}
